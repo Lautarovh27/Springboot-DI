e8a167e6a0e9dd5fa7f64fe049d08ecebcaaa132
Lautaro <lautarovanh@gmail.com>
2026-05-09 14:43:24 -0300

    Se agrego un Controller standard

---
+++ src/main/java/com/lautaro/springboot/di/app/springboot_di/controllers/SomeController.java
@@ -0,0 +1,35 @@
+package com.lautaro.springboot.di.app.springboot_di.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lautaro.springboot.di.app.springboot_di.models.Product;
+import com.lautaro.springboot.di.app.springboot_di.services.ProductService;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RestController
+@RequestMapping("/api")
+public class SomeController {
+
+    private ProductService service = new ProductService();
+    
+    @GetMapping   
+    public List<Product> list(){
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product show(@PathVariable Long id) {
+        return service.findById(id);
+    }
+}
