4acc8ab3c74b64925aa3cab6c698fbd068912450
Lautaro <lautarovanh@gmail.com>
2026-05-09 17:28:17 -0300

    se utilizo Cloneable para no modificar el producto original

--- src/main/java/com/lautaro/springboot/di/app/springboot_di/services/ProductService.java
+++ src/main/java/com/lautaro/springboot/di/app/springboot_di/services/ProductService.java
@@ -14,7 +14,9 @@ public class ProductService {
     public List<Product> findAll() {
         return repository.findAll().stream().map(p -> {
             Double priceTax = p.getPrice() * 1.25d;
-            Product newProduct = new Product(p.getId(), p.getName(), priceTax.longValue());
+            //Product newProduct = new Product(p.getId(), p.getName(), priceTax.longValue());
+            Product newProduct = (Product) p.clone(); //clonamos el producto para no modificar el original
+            newProduct.setPrice(priceTax.longValue());
             return newProduct;
         }).collect(Collectors.toList());
     }
