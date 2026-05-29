b0b2eb803b0992ed92ebc3666d2cc9eea6df51c4
Lautaro <lautarovanh@gmail.com>
2026-05-29 12:41:09 -0300

    Se agrego @SessionScope

--- src/main/java/com/lautaro/springboot/di/app/springboot_di/services/ProductServiceImpl.java
+++ src/main/java/com/lautaro/springboot/di/app/springboot_di/services/ProductServiceImpl.java
@@ -28,9 +28,11 @@ public class ProductServiceImpl implements ProductService {
         return repository.findAll().stream().map(p -> {
             Double priceTax = p.getPrice() * 1.25d;
             //Product newProduct = new Product(p.getId(), p.getName(), priceTax.longValue());
-            Product newProduct = (Product) p.clone(); //clonamos el producto para no modificar el original
-            newProduct.setPrice(priceTax.longValue());
-            return newProduct;
+            //Product newProduct = (Product) p.clone(); //clonamos el producto para no modificar el original
+            //newProduct.setPrice(priceTax.longValue());
+            //return newProduct;
+            p.setPrice(priceTax.longValue());
+            return p;
         }).collect(Collectors.toList());
     }
     
